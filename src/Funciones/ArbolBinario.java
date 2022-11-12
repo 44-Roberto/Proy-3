88f6838da23cf877e10b8b533450f357ae0abf73
ErwinLima <ealiman@correo.url.edu.gt>
2022-11-11 23:39:18 -0600

    Metodo de busca añadido

--- src/Funciones/ArbolBinario.java
+++ src/Funciones/ArbolBinario.java
@@ -21,12 +21,56 @@ import java.util.logging.Logger;
 public class ArbolBinario {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
     
-    public String Search(String id, String master)
+    public void Add(String key, String info, String pathMaster, String descriptor, int keySize){
+        File master = new File(pathMaster);
+        
+    }
+    
+    public String Search(String id, String master, int keySize)
     {
+        String resultado = "null";
         ArrayList<String> lineas = getAll(master);
+        if (lineas.isEmpty()) {
+            return resultado;
+        }
         String reg = lineas.get(0);
-        boolean flag = false;
-        return "null";
+        var temp = reg.split("[|]");
+        String key = getKey(reg, keySize);
+        while(id.compareTo(key) != 0){            
+            int p;
+            if(id.compareTo(key) < 0){
+                if (temp[0].equals("null")) {
+                    return "null";
+                }else{
+                    p = Integer.parseInt(temp[0]);
+                    reg = lineas.get(p - 1);
+                    temp = reg.split("[|]");
+                    key = getKey(reg, keySize);
+                }
+            }else{
+                if (temp[1].equals("null")) {
+                    return "null";
+                }else{
+                    p = Integer.parseInt(temp[1]);
+                    reg = lineas.get(p - 1);
+                    temp = reg.split("[|]");
+                    key = getKey(reg, keySize);
+                }
+            }
+        }
+        return reg;        
+    }
+    
+    private String getKey(String reg, int keySize){
+        var temp = reg.split("[|]");        
+        String[] aux = new String[keySize];
+        int c = 0;
+        while(c < keySize){
+            aux[c] = temp[c+2];
+            c++;
+        }
+        String key = String.join("|", aux);
+        return key;
     }
     
     public ArrayList<String> getAll(String pathMaster){
